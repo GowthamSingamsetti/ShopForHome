fc93da2f3a2e0b7230db32f0b78cfec7a4f43ebf
Gowtham Singamsetti <77894774+GowthamSingamsetti@users.noreply.github.com>
2022-08-07 23:57:37 +0530

    Add files via upload

---
+++ Other Entities/src/main/java/com/shopforhome/Shopforhome1Application.java
@@ -0,0 +1,13 @@
+package com.shopforhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shopforhome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shopforhome1Application.class, args);
+	}
+
+}
